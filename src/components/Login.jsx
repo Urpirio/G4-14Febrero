409e8258a129632dd67274ef82df5ff35ce95fc9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-03 21:18:56 -0400

    Update Prueba Admin.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,29 @@ export default function Login() {
         e.preventDefault()
         if(DataUser[0].Username == document.getElementById('Username').value){
             if(DataUser[0].Password == document.getElementById('Password').value){
-                window.location.href = '/Admin';
+                document.getElementById('SL__Form').style.display = 'none';
+                document.getElementById('GroupStudent').style.display = 'flex';
+                document.getElementById('GroupStudent').innerHTML = `<h1 class = 'GSwelcome'>
+                 Welcome <span>${document.getElementById('Username').value}</span> to <span>G4</span></h1>`;
+
+                let D = document.createElement('button')
+                D.className = 'btnContinuar';
+
+                D.addEventListener('click',()=>{
+
+                    document.getElementById('GroupStudent').style.display = 'none';
+                    document.getElementById('SCartas').style.display = 'flex';
+
+                    document.getElementById('SCartas').innerHTML = ` <div >
+            <h2>${document.getElementById('Username').value}</h2>
+            <p>Aprecio profundamente tu paciencia y la manera en que has compartido tu experiencia conmigo. Me has mostrado que la verdadera enseñanza va más allá de los conocimientos; se trata de inspirar y motivar</p>
+                   </div>`
+                });
+
+
+                let T = document.createTextNode('Ver Carta');
+                D.append(T)
+                document.getElementById('GroupStudent').append(D);
                 
             }else{
 
